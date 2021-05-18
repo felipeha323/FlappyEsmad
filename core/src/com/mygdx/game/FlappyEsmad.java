b95166e8f9710a2731dce644771789f4e7b1d60e
felipeha323 <60258356+felipeha323@users.noreply.github.com>
2021-05-17 23:36:46 -0500

    capuchos

--- core/src/com/mygdx/game/FlappyEsmad.java
+++ core/src/com/mygdx/game/FlappyEsmad.java
@@ -9,24 +9,36 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class FlappyEsmad extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
+	Texture [] capuchos;
+	int flapState=0;
 
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("fondo.jpg");
+		capuchos = new Texture[2];
+		capuchos[0] = new Texture("capucho.png");
+		capuchos[1] = new Texture("capucho_2.png");
 	}
 
 	@Override
 	public void render () {
+		if (flapState==0){
+			flapState=1;
+		}else {
+			flapState=0;
+		}
 	batch.begin();
 	batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+	batch.draw(capuchos[flapState],Gdx.graphics.getWidth()/2 - capuchos[flapState].getWidth()/2,
+			Gdx.graphics.getHeight()/2 - capuchos[flapState].getHeight()/2);
 	batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-	
+
 	}
 }
