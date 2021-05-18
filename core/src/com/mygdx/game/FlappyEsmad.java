c2eb2580c1c0ef5c203ccc96291b4d197cbf9982
felipeha323 <60258356+felipeha323@users.noreply.github.com>
2021-05-18 00:31:14 -0500

    Animación y edificios

--- core/src/com/mygdx/game/FlappyEsmad.java
+++ core/src/com/mygdx/game/FlappyEsmad.java
@@ -11,6 +11,13 @@ public class FlappyEsmad extends ApplicationAdapter {
 	Texture background;
 	Texture [] capuchos;
 	int flapState=0;
+	float capuchoY;
+	int gameState=0;
+	float velocity=0;
+	float gravity=2;
+	Texture toptube;
+	Texture bottomTube;
+	float gap=400;
 
 	
 	@Override
@@ -20,10 +27,37 @@ public class FlappyEsmad extends ApplicationAdapter {
 		capuchos = new Texture[2];
 		capuchos[0] = new Texture("capucho.png");
 		capuchos[1] = new Texture("capucho_2.png");
+		bottomTube= new Texture("bottomtube.png");
+		toptube= new Texture("toptube.png");
+		capuchoY=Gdx.graphics.getHeight()/2 - capuchos[flapState].getHeight()/2;
 	}
 
 	@Override
 	public void render () {
+
+		if(gameState!=0){
+
+
+			if (Gdx.input.justTouched()){
+				velocity=-30;
+			}
+			if (capuchoY>0 || velocity < 0){
+				velocity += gravity;
+				capuchoY -= velocity;
+			}
+
+			if (flapState==0){
+				flapState=1;
+			}else {
+				flapState=0;
+			}
+
+
+		}else{
+			if(Gdx.input.justTouched()){
+				gameState=1;
+			}
+		}
 		if (flapState==0){
 			flapState=1;
 		}else {
@@ -31,8 +65,13 @@ public class FlappyEsmad extends ApplicationAdapter {
 		}
 	batch.begin();
 	batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+batch.draw(toptube,Gdx.graphics.getWidth()/2 - toptube.getWidth()/2,
+		Gdx.graphics.getHeight()/2 - toptube.getHeight()/2);
+
+
 	batch.draw(capuchos[flapState],Gdx.graphics.getWidth()/2 - capuchos[flapState].getWidth()/2,
-			Gdx.graphics.getHeight()/2 - capuchos[flapState].getHeight()/2);
+			capuchoY);
 	batch.end();
 	}
 	
