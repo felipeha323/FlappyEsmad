080efc689feccf51ee3392f6eff9fdd42c4e3cd4
felipeha323 <60258356+felipeha323@users.noreply.github.com>
2021-05-18 01:21:22 -0500

    random edificios

--- core/src/com/mygdx/game/FlappyEsmad.java
+++ core/src/com/mygdx/game/FlappyEsmad.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class FlappyEsmad extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
@@ -17,7 +19,14 @@ public class FlappyEsmad extends ApplicationAdapter {
 	float gravity=2;
 	Texture toptube;
 	Texture bottomTube;
+	int numberOfTube=4;
 	float gap=400;
+	float[] tubeOffset= new float[numberOfTube];
+	Random random= new Random();
+	float[] tubeX=new float[numberOfTube];
+	float tubeVelocity=4;
+
+	float distanceBetweenTubes;
 
 	
 	@Override
@@ -29,21 +38,44 @@ public class FlappyEsmad extends ApplicationAdapter {
 		capuchos[1] = new Texture("capucho_2.png");
 		bottomTube= new Texture("bottomtube.png");
 		toptube= new Texture("toptube.png");
+
 		capuchoY=Gdx.graphics.getHeight()/2 - capuchos[flapState].getHeight()/2;
+		distanceBetweenTubes = Gdx.graphics.getWidth()/2;
+
+		for (int i =0; i< numberOfTube; i++){
+
+			tubeOffset[i]=(random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
+			tubeX[i] = Gdx.graphics.getWidth()/2 - toptube.getWidth()/2 + i * distanceBetweenTubes;
+		}
 	}
 
 	@Override
 	public void render () {
+		batch.begin();
+		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
 		if(gameState!=0){
 
 
 			if (Gdx.input.justTouched()){
 				velocity=-30;
+
 			}
+
+			for (int i=0; i < numberOfTube; i++){
+				tubeX[i] -= tubeVelocity;
+
+				batch.draw(toptube,tubeX[i],
+						Gdx.graphics.getHeight()/2  + gap / 2 +tubeOffset[i]);
+
+				batch.draw(bottomTube,tubeX[i],
+						Gdx.graphics.getHeight()/2 -gap/2 - bottomTube.getHeight() + tubeOffset[i]);
+			}
+
 			if (capuchoY>0 || velocity < 0){
 				velocity += gravity;
 				capuchoY -= velocity;
+
 			}
 
 			if (flapState==0){
@@ -63,14 +95,9 @@ public class FlappyEsmad extends ApplicationAdapter {
 		}else {
 			flapState=0;
 		}
-	batch.begin();
-	batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
-batch.draw(toptube,Gdx.graphics.getWidth()/2 - toptube.getWidth()/2,
-		Gdx.graphics.getHeight()/2  + gap / 2);
 
-batch.draw(bottomTube,Gdx.graphics.getWidth()/2 - bottomTube.getWidth() / 2,
-		Gdx.graphics.getHeight()/2 -gap/2 - bottomTube.getHeight());
+
 
 
 	batch.draw(capuchos[flapState],Gdx.graphics.getWidth()/2 - capuchos[flapState].getWidth()/2,
