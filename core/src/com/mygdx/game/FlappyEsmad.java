c47e623a2edbafe9612ec3e2375f5947bcc4e2aa
William1405 <61747355+William1405@users.noreply.github.com>
2021-05-27 12:46:21 -0500

    Cambios finales

--- core/src/com/mygdx/game/FlappyEsmad.java
+++ core/src/com/mygdx/game/FlappyEsmad.java
@@ -31,21 +31,24 @@ public class FlappyEsmad extends ApplicationAdapter {
 	Random random= new Random();
 	float[] tubeX=new float[numberOfTube];
 	float tubeVelocity=3;
-
 	float distanceBetweenTubes;
 	Texture gameOver;
-
 	int score=0;
 	int scoringTube=0;
 	BitmapFont bitmapFont;
-
 	ShapeRenderer shapeRenderer;
 	Circle circle;
 	Rectangle[] topTubeRectangle;
 	Rectangle[] bottomTubeRectangle;
 
-
-	
+	public void createTub(){
+		for (int i =0; i< numberOfTube; i++){
+			tubeOffset[i]=(random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
+			tubeX[i] =(float) Gdx.graphics.getWidth()/2 - toptube.getWidth()/2 +Gdx.graphics.getWidth()+ i * distanceBetweenTubes;
+			topTubeRectangle[i] = new Rectangle();
+			bottomTubeRectangle[i] = new Rectangle();
+		}
+	}
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -56,59 +59,33 @@ public class FlappyEsmad extends ApplicationAdapter {
 		bottomTube= new Texture("bottomtube.png");
 		toptube= new Texture("toptube.png");
 		gameOver = new Texture("fin.png");
-
 		bitmapFont =new BitmapFont();
 		bitmapFont.setColor(Color.WHITE);
 		bitmapFont.getData().scale(5);
-
 		shapeRenderer = new ShapeRenderer();
 		circle = new Circle();
-
 		topTubeRectangle = new Rectangle[numberOfTube];
 		bottomTubeRectangle = new Rectangle[numberOfTube];
 		distanceBetweenTubes = (float) (Gdx.graphics.getWidth());
-
 		capuchoY=Gdx.graphics.getHeight()/2 - capuchos[flapState].getHeight()/2;
-
-
-		for (int i =0; i< numberOfTube; i++){
-
-			tubeOffset[i]=(random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
-			tubeX[i] =(float) Gdx.graphics.getWidth()/2 - toptube.getWidth()/2 +Gdx.graphics.getWidth()+ i * distanceBetweenTubes;
-			topTubeRectangle[i] = new Rectangle();
-			bottomTubeRectangle[i] = new Rectangle();
-		}
+		createTub();
 	}
+
 	public void gameStart(){
 		capuchoY=Gdx.graphics.getHeight()/2 - capuchos[flapState].getHeight()/2;
 		scoringTube=0;
 		score=0;
 		velocity=0;
-		for (int i =0; i< numberOfTube; i++){
-
-
-
-
-			tubeOffset[i]=(random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
-			tubeX[i] = Gdx.graphics.getWidth()/2 - toptube.getWidth()/2+ Gdx.graphics.getWidth()+ i * distanceBetweenTubes;
-			topTubeRectangle[i] = new Rectangle();
-			bottomTubeRectangle[i] = new Rectangle();
-		}
-
+		createTub();
 	}
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-
 		if(gameState==1){
-
-
 			if (Gdx.input.justTouched()){
 				velocity=-30;
-
 			}
-
 			if (tubeX[scoringTube] < Gdx.graphics.getWidth()/2){
 				score++;
 				if (scoringTube < numberOfTube - 1){
@@ -118,7 +95,6 @@ public class FlappyEsmad extends ApplicationAdapter {
 				}
 
 			}
-
 			for (int i=0; i < numberOfTube; i++){
 				if(tubeX[i] < -toptube.getWidth()){
 
@@ -127,9 +103,6 @@ public class FlappyEsmad extends ApplicationAdapter {
 				} else {
 					tubeX[i] -= tubeVelocity;
 				}
-
-
-
 				batch.draw(toptube,tubeX[i],
 						Gdx.graphics.getHeight()/2  + gap / 2 +tubeOffset[i]);
 
@@ -145,74 +118,42 @@ public class FlappyEsmad extends ApplicationAdapter {
 						bottomTube.getWidth(),
 						bottomTube.getHeight());
 			}
-
 			if (capuchoY>0){
 				velocity += gravity;
 				capuchoY -= velocity;
-
 			} else {
 				gameState=2;
 			}
-
 			if (flapState == 0) {
 				flapState = 1;
 			} else {
 				flapState = 0;
 			}
-
 		} else if(gameState == 0) {
 
 			if (Gdx.input.justTouched()) {
 				gameState = 1;
 			}
-
 		} else if (gameState == 2) {
-
 			batch.draw(gameOver,Gdx.graphics.getWidth() / 2 - gameOver.getWidth() / 2,
 					Gdx.graphics.getHeight() / 2 - gameOver.getHeight() / 2);
-
 			if (Gdx.input.justTouched()) {
-
 				gameState = 1;
-
 				gameStart();
-
 			}
-
 		}
-
 		batch.draw(capuchos[flapState],Gdx.graphics.getWidth() / 2 - capuchos[flapState].getWidth() / 2 ,
 				capuchoY);
-
 		bitmapFont.draw(batch,Integer.toString(score),150,150);
-
 		batch.end();
-
 		circle.set(Gdx.graphics.getWidth() / 2
 				,capuchoY + capuchos[flapState].getWidth() / 2
 				,capuchos[flapState].getWidth() / 2);
-
-		//shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		//shapeRenderer.setColor(Color.RED);
-		//shapeRenderer.circle(circle.x,circle.y,circle.radius);
 		for (int i = 0 ; i < numberOfTube ; i++) {
-			/*
-			shapeRenderer.rect(tubeX[i],
-					Gdx.graphics.getHeight() / 2  + gap / 2 + tubeOffset[i],
-					topTube.getWidth(),
-					topTube.getHeight());
-			shapeRenderer.rect(tubeX[i],
-					Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i],
-					bottomTube.getWidth(),
-					bottomTube.getHeight());
-			*/
 			if (Intersector.overlaps(circle,topTubeRectangle[i])
 					|| Intersector.overlaps(circle,bottomTubeRectangle[i]) ) {
-
 				gameState = 2;
-
 			}
-
 		}
 		shapeRenderer.end();
 
